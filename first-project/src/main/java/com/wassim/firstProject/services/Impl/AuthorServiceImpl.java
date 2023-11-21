f26682d506b9a6829b89654b9e184083e9b2a903
Wassim <wassimsoussi.ws@gmail.com>
2023-11-21 20:20:43 +0100

    Implementing Project Lombok

--- first-project/src/main/java/com/wassim/firstProject/services/Impl/AuthorServiceImpl.java
+++ first-project/src/main/java/com/wassim/firstProject/services/Impl/AuthorServiceImpl.java
@@ -3,18 +3,16 @@ package com.wassim.firstProject.services.Impl;
 import com.wassim.firstProject.entities.Author;
 import com.wassim.firstProject.repositories.AuthorRepository;
 import com.wassim.firstProject.services.AuthorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorRepository authorRepository;
-
-    public AuthorServiceImpl(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }
 
     @Override
     public Author addAuthor(Author author) {
--- first-project/src/main/java/com/wassim/firstProject/services/Impl/BookServiceImpl.java
+++ first-project/src/main/java/com/wassim/firstProject/services/Impl/BookServiceImpl.java
@@ -3,18 +3,16 @@ package com.wassim.firstProject.services.Impl;
 import com.wassim.firstProject.entities.Book;
 import com.wassim.firstProject.repositories.BookRepository;
 import com.wassim.firstProject.services.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
-
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @Override
     public Book addBook(Book book) {
